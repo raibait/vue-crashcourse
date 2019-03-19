de23b240a6da5c61aff8f093d19e1a85bc22d9c0
raibait <38789839+raibait@users.noreply.github.com>
2019-03-19 21:32:01 +0100

    next commit

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 new Vue({
 	el: '#vueApp',
 	data: {
-		message: 'Hello Vue!',
-	},	
+		message: 'Hello Vue!'
+	},
 });
 
 new Vue({
-	el: 'span',
+	el: 'div span',
 	data: {
 		message: 'Hello Vue!',
 	},
 });
 
+// properties 'v-bind:' = ':'
+// events 'v-on' = '@'
 new Vue({
-	template:"",
 	el: '#vueAppBinds',
 	data: {
-		message: 'Hello Vue!',
+		title: 'Hello Vue!',
+		style: {
+			color: 'red',
+			'font-size': '25px'
+		},
+		enabled: true
 	},
+	methods: {
+		alert: function() {
+			window.alert('It works!')
+		},
+
+		alertData: function() {
+			window.alert('the title is ' + this.title + ':)')
+		},
+
+		changeColor: function() {
+			if(this.style.color ==='red') {
+				this.style.color = 'blue'
+			} else {
+				this.style.color = 'red'
+			}
+		},
+
+		toggleEnable: function() {
+			this.enabled = !this.enabled;
+		}
+
+	}
 });
