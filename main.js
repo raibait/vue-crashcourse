71ca94ec0f315a4fe779eb211e54a84ce8e83207
Admin <admin@Air-Admin.GlobeGroup>
2019-03-19 18:07:51 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,21 @@
+new Vue({
+	el: '#vueApp',
+	data: {
+		message: 'Hello Vue!',
+	},	
+});
+
+new Vue({
+	el: 'span',
+	data: {
+		message: 'Hello Vue!',
+	},
+});
+
+new Vue({
+	template:"",
+	el: '#vueAppBinds',
+	data: {
+		message: 'Hello Vue!',
+	},
+});
